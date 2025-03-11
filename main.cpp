e5f8dfba8c44682ffc21a3a6e3496576a03b255e
cf2031293 <chenf1@ufl.edu>
2025-03-11 14:22:07 -0400

    Add binary file reading functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
-
+#include <vector>
+#include <string>
+using namespace std;
 int main() {
     // Open file in binary input mode
     std::fstream file("tips.shp", std::ios_base::binary | std::ios_base::in);
@@ -13,11 +15,31 @@ int main() {
     unsigned int num_strings;
     // TODO: Read number of strings
     // Hint: file.read((char *)&num_strings, sizeof(num_strings));
+    file.read((char *)&num_strings, sizeof(num_strings));
+    std::cout << "Number of strings: " << num_strings << std::endl;
+
 
     // TODO: Add loop to:
     // 1. Read string length
     // 2. Read string characters
     // 3. Print string
+    for (unsigned int i = 0; i < num_strings; i++) {
+        unsigned int str_len;
+
+        // Read length of string (including null terminator)
+        file.read((char *)&str_len, sizeof(str_len));
+
+        // Create buffer and read string
+        char* buffer = new char[str_len];
+        file.read(buffer, str_len);
+
+        // Print string (stop at null terminator)
+        std::cout << "String " << (i + 1) << " (length " << (str_len - 1)
+                  << "): " << buffer << std::endl;
+
+        // Clean up
+        delete[] buffer;
+    }
 
     file.close();
     return 0;
